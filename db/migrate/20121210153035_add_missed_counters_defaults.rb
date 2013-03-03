c2dfd26883de0b1fb2d5f0037f825959efb12d77
Bartosz Myszko <bart.mouse@gmail.com>
2013-03-03 01:35:19 +0100

    data integrity for Idea counters

--- db/migrate/20121210153035_add_missed_counters_defaults.rb
+++ db/migrate/20121210153035_add_missed_counters_defaults.rb
@@ -1,5 +1,10 @@
 class AddMissedCountersDefaults < ActiveRecord::Migration
   def change
+    Idea.where('ideas.additional_signatures_count IS NULL OR ideas.target_count IS NULL').find_each { |i|
+      i.additional_signatures_count = 0 unless i.additional_signatures_count
+      i.target_count = 0 unless i.target_count
+      i.save
+    }
     change_column :ideas, :additional_signatures_count, :integer, default: 0
     change_column :ideas, :target_count, :integer, default: 0
   end
