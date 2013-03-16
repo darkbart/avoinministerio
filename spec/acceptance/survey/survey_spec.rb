469bcfdc8554f92e1ef4ff98a4996ca4404c7e3f
Bartosz Myszko <bart.mouse@gmail.com>
2013-03-16 20:46:14 +0100

    fixed survey spec

--- spec/acceptance/survey/survey_spec.rb
+++ spec/acceptance/survey/survey_spec.rb
@@ -1,24 +1,32 @@
 #encoding: utf-8
 
+require 'rake'
 require "acceptance/acceptance_helper"
 
 Capybara.javascript_driver = :webkit
+
 feature "Survey" do
+
+  before :each do
+    AvoinMinisterio::Application.load_tasks
+    ENV['FILE']="surveys/avoin-ministerio-attitudes-1-fi.rb"
+    Rake::Task[:'surveyor'].reenable
+    Rake::Task[:'surveyor'].invoke
+  end
 
   scenario "User should be to go to survey after sign_up" do
     visit signup_page
     fill_in "Sähköposti", :with => "citizen-kane@example.com"
-    fill_in "Salasana", :with => "salainensana12345"
-    fill_in "Salasanan vahvistus", :with => "salainensana12345"
+    fill_in "citizen_password", :with => "salainensana12345"
+    fill_in "citizen_password_confirmation", :with => "salainensana12345"
     fill_in "Kutsumanimi", :with => "Cit-Kane"
     fill_in "Etunimet", :with => "Kane City"
     fill_in "Sukunimi", :with => "Citizen"
     click_button "Rekisteröidy"
 
     should_be_on after_sign_up
-    click_on "Osallistu tieteelliseen tutkimukseen -- täytä kysely"
-    page.should have_content('#surveyor')
-    should_be_on take_survey_path(:survey_code => SURVEY_ACCESS_CODE[:fi])
+    first(:button, 'Osallistu tieteelliseen tutkimukseen -- täytä kysely').click
+    page.has_css?('div#surveyor')
   end
 
 end
