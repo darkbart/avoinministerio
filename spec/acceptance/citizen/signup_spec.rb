7f62b5c4fe606e28c289cfe447371076baa8aa35
Bartosz Myszko <bart.mouse@gmail.com>
2013-03-16 16:29:00 +0100

    fixed citizen signup test - capybara 2

--- spec/acceptance/citizen/signup_spec.rb
+++ spec/acceptance/citizen/signup_spec.rb
@@ -9,8 +9,8 @@ feature "Citizen Signup" do
   scenario "Signup" do
     visit signup_page
     fill_in "Sähköposti", :with => "citizen-kane@example.com"
-    fill_in "Salasana", :with => "salainensana12345"
-    fill_in "Salasanan vahvistus", :with => "salainensana12345"
+    fill_in "citizen_password", :with => "salainensana12345"
+    fill_in "citizen_password_confirmation", :with => "salainensana12345"
     fill_in "Kutsumanimi", :with => "Cit-Kane"
     fill_in "Etunimet", :with => "Kane City"
     fill_in "Sukunimi", :with => "Citizen"
